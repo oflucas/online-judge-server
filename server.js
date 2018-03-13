1581f429abe9abd375262854190f8a37d42a3896
Lucas LI <scofieldgood@gmail.com>
2018-03-14 02:02:33 +0800

    F/E connect to B/E: send index.html to browser at first

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var express = require("express");
 const app = express()
 var restRouter = require("./routes/rest");
+var indexRouter = require("./routes/index");
+var path = require('path');
 var mongoose = require("mongoose");
 
 // mongodb://<dbuser>:<dbpassword>@ds213259.mlab.com:13259/oj
 // <dbuser> == user, <dbpassword> == user
 mongoose.connect("mongodb://user:user@ds213259.mlab.com:13259/oj");
 
-// this is actually a router
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/', indexRouter)
 
 app.use("/api/v1", restRouter);
 
