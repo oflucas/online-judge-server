9d29d08196f598f570030976401c0a77a80c2211
Lucas LI <scofieldgood@gmail.com>
2018-03-18 22:29:07 +0800

    enable editor change socket communitation! 2

--- services/socketService.js
+++ services/socketService.js
@@ -1,14 +1,38 @@
 module.exports = function(io) {
-  // receive
-  io.on('connection', (socket) => {
-    console.log(socket);
+    // collaboration sessionsId map
+    var collaborations = [];
+    // map from socketId to sessionId
+    var socketIdToSessionId = [];
 
-    // handshake to get some identity info
-    var message = socket.handshake.query['message'];
-    console.log(message);
+    io.on('connection', socket => {
+        let sessionId = socket.handshake.query['sessionId'];
 
-    // response
-    //  socket.id is to specify receiver
-    io.to(socket.id).emit('message', 'hehe from server');
-  });
+        socketIdToSessionId[socket.id] = sessionId;
+        // add socket.id to corresponding collaboration session participants
+        if (!(sessionId in collaborations)) {
+          console.log("creating new session");
+          collaborations[sessionId] = {
+            'participants': []
+          };
+        }
+        collaborations[sessionId]['participants'].push(socket.id);
+
+
+        // socket event listeners
+        socket.on('change', delta => {
+            console.log( "change " + socketIdToSessionId[socket.id] + " " + delta ) ;
+            let sessionId = socketIdToSessionId[socket.id];
+
+            if (sessionId in collaborations) {
+              let participants = collaborations[sessionId]['participants'];
+              for (let i = 0; i < participants.length; i++) {
+                if (socket.id != participants[i]) {
+                  io.to(participants[i]).emit('change', delta);
+                }
+              }
+            } else {
+              console.warn('could not tie socket.id to any collaboration');
+            }
+        });
+      });
 }
