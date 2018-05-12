84860b61fd321f5bcabfc53c08e7c2f897238002
Lucas Li <scofieldgood@gmail.com>
2018-05-12 15:12:02 +0800

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -5,15 +5,20 @@ fuser -k 3000/tcp
 fuser -k 5000/tcp
 
 service redis_6379 start
-cd ./oj-server
+
+# In online-judge-server/
 npm install
 nodemon server.js &
-cd ../oj-client
+
+# In online-judge-client/
+cd ../online-judge-client
 npm install
 ng build --watch
 
+# In online-judge-executor/
+cd ../online-judge-executor
 pip install -r requirements.txt
-python executer_server.py
+python executer_server.py 5000
 
 echo "=================================================="
 read -p "PRESS [ENTER] TO TERMINATE PROCESSES." PRESSKEY
